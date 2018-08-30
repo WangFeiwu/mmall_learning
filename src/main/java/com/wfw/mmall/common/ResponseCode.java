324b72f1dd9cd9382cdd5b64e9a89976fe06dd6c
wfw <wfw@wfw.com>
2018-08-30 14:13:55 +0800

    8-30repu

--- src/main/java/com/wfw/mmall/common/ResponseCode.java
+++ src/main/java/com/wfw/mmall/common/ResponseCode.java
@@ -14,7 +14,6 @@ public enum ResponseCode {
 	private final String desc;
 	
 	ResponseCode(int code,String desc) {
-		// TODO Auto-generated constructor stub
 		this.code=code;
 		this.desc=desc;
 	}
